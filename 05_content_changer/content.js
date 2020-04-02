4eba2481bcf94b4c9160d51f083b861559fbf44b
Alisha Dhir <alishadhir@Second-Gina.fios-router.home>
2020-04-02 16:08:18 -0400

    started coding

---
+++ 05_content_changer/content.js
@@ -0,0 +1,24 @@
+
+// function swapImg(img) {
+//   var newimg = chrome.extension.getURL("images/rainbow.png");
+//   img.src = newimg;
+// }
+//
+// // Look for a "repository" description
+// var descriptions = document.getElementsByClassName('repository-description');
+//
+// // This is just a silly algorithm that reverses the text
+// for (var k = 0; k < descriptions.length; k++) {
+//   // Look at the text
+//   var txt = descriptions[k].innerHTML;
+//   // Split it up
+//   var words = txt.split(/\s+/);
+//   // Reverse the array
+//   words = words.reverse();
+//   // Join it back together
+//   txt = words.join(' ');
+//   // Set the new text and change the background-color
+//   descriptions[k].innerHTML = txt;
+//   descriptions[k].style['background-color'] = '#F0C';
+// }
+console.log("it working - Background Script is Running");
