99632a909c1f7ce72f60195e68615f663df9582d
Alisha Dhir <alishadhir@Second-Gina.fios-router.home>
2020-04-06 21:09:29 -0400

    finally works

---
+++ imageSwap-Aisha/swap.js
@@ -0,0 +1,19 @@
+﻿var images = [
+    "https://media.giphy.com/media/26gsdS1KCyxwTl6IU/source.gif",
+    "https://media.giphy.com/media/Igiy7FvrsJLr3KFEUL/source.gif",
+    "https://media.giphy.com/media/3o7WIHtdLD2fXURIFG/source.gif"
+];
+window.onload = replaceImages;
+
+function replaceImages() {
+    for (var i in document.images) {
+        var targetImage = document.images[i];
+        var randomImage = images[Math.floor(Math.random() * images.length)];
+        var height = targetImage.height;
+        var width = targetImage.width;
+
+        targetImage.src = randomImage;
+        targetImage.height = height;
+        targetImage.width = width;
+    }
+}
